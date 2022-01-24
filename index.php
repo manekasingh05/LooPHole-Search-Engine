573e0506750bfe652b854711d825071103ce8a44
manekasingh <77896836+manekasingh@users.noreply.github.com>
2022-01-25 04:51:03 +0530

    initial-commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to LooPHole</title>
+
+	<meta name="description" content="Search different content provided by the user">
+	<meta name="keywords" content="Search engine, LooPHole, websites, content, images, writeups">
+	<meta name="author" content="ManekaSingh">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+
+	<div class="wrapper indexPage">
+	
+
+		<div class="mainSection">
+
+			<div class="logoContainer">
+				<img src="assets/images/search.jpg" title="Logo of our site" alt="Site logo">
+			</div>
+
+
+			<div class="searchContainer">
+
+				<form action="search.php" method="GET">
+
+					<input class="searchBox" type="text" name="term" autocomplete="off">
+					<input class="searchButton" type="submit" value="Search">
+
+
+				</form>
+
+			</div>
+
+
+		</div>
+
+
+	</div>
+
+</body>
+</html>
